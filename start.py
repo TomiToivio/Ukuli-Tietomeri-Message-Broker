69aecb121eda1638d52f9824be940f27346081e7
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-07-24 22:22:55 +0000

    Requirements

--- start.py
+++ start.py
@@ -1,2 +1,7 @@
 import sentry_sdk
 sentry_sdk.init("https://f31c9903bb2f41be9d6131da5d7db0a8@o325237.ingest.sentry.io/1829931")
+
+import rollbar
+rollbar.init('6332cfdfea4a4b608bbb09002bc1c260')
+
+
