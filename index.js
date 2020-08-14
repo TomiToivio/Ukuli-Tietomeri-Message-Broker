709837fa216d86084e473b044725974d678e803a
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-14 22:16:34 +0300

    Mongo
    MongoDB

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const mongo_host = config.get('mongo.host');
 const mongo_port = config.get('mongo.port');
 const mongo_database = config.get('mongo.database');
 
-const mongo_url = 'mongodb://' + mongo_user + ':' + mongo_password + '@' + mongo_host + ':' + mongo_port + '/' + mongo_database; 
-const MongoClient = require('mongodb').MongoClient(mongo_url);
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const mongo_url = 'mongodb://' + mongo_user + ':' + mongo_password + '@' + mongo_host + ':' + mongo_port; 
+const test = require('assert');
 
 mqtt_client.on('connect', function () {
     client.subscribe("ukuli/#");
@@ -30,5 +30,13 @@ client.on('message', function (topic, message) {
 });
 
 function mongodbSave(topic,message) {
-
+    MongoClient.connect(mongo_url, function(err, client) {
+        const db = client.db(mongo_database);
+        db.collection('inserts').insertOne({a:1}, function(err, r) {
+          assert.equal(null, err);
+          assert.equal(1, r.insertedCount);
+          insertOne(doc, options);
+        });
+        client.close();
+    });    
 }
